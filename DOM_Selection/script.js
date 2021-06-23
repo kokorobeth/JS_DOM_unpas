c35ce0c60f8be2144910943507a3c5efadea07a9
Aziz Kurniawan <aziz.kurniawan13@gmail.com>
2021-06-24 00:30:18 +0700

    update files

--- DOM_Selection/script.js
+++ DOM_Selection/script.js
@@ -11,3 +11,13 @@ const p = document.getElementsByTagName('p');
 for(let i = 0; i < p.length; i++) {
     p[i].style.backgroundColor = 'lightblue';
 }
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '50px';
+
+//document.getElementsByClassName
+// -> HTMLCollection
+
+const p2 = document.getElementsByClassName('p2')[0];
+
+p2.innerHTML = 'Ini diubah menjadi JavaScript';
