a112a4ce654004bbca70321d7474514050afa4fa
Aziz Kurniawan <aziz.kurniawan13@gmail.com>
2021-06-26 13:56:22 +0700

    add file dom_Js_manipulation

---
+++ DOM_Manipulation/script.js
@@ -0,0 +1,6 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<em>Aziz Kurniawan</em>';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'Hello World!';
+
