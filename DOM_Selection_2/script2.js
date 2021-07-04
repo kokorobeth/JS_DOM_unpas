a1a1fbcb11a5a85427a57950bcd7deb06a6b0cc3
Aziz Kurniawan <aziz.kurniawan13@gmail.com>
2021-07-04 14:08:35 +0700

    add file latihan dom selection 2

---
+++ DOM_Selection_2/script2.js
@@ -0,0 +1,18 @@
+const judul = document.getElementById('judul');
+judul.style.color = "blue";
+judul.style.backgroundColor = "pink";
+judul.innerHTML = "Aziz Kurniawan";
+
+const p = document.querySelectorAll('p');
+for(let i = 0; i < p.length; i++) {
+    p[i].style.backgroundColor = "lightblue";
+}
+
+const li2 = document.querySelector("section#b ul li:nth-child(2)");
+li2.style.fontSize = "35px";
+li2.style.backgroundColor = "lightblue";
+
+const sectionB = document.querySelector("section#b");
+const p4 = sectionB.getElementsByTagName('p')[0];
+p4.style.backgroundColor = 'lightgreen';
+p4.style.color = 'yellow';
