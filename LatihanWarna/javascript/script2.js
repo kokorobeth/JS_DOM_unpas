84af3419dbad953718cedb48a0684ba948313cf6
Aziz Kurniawan <aziz.kurniawan13@gmail.com>
2021-07-11 11:08:29 +0700

    update files lat warna js-dom

--- LatihanWarna/javascript/script2.js
+++ LatihanWarna/javascript/script2.js
@@ -68,3 +68,31 @@ randomColor.addEventListener('click', function() {
 
     document.body.style.backgroundColor = 'rgb('+ r +','+ g +', '+ b +')';
 });
+
+const sMerah = document.querySelector('input[name=sMerah]');
+const sHijau = document.querySelector('input[name=sHijau]');
+const sBiru = document.querySelector('input[name=sBiru]');
+
+sMerah.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +', 100)';
+});
+
+sHijau.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +', 100)';
+});
+
+sBiru.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +', '+ b +')';
+});
